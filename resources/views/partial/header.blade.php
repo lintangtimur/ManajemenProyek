45d3b4ef640c4e9031932fb2d88e9ce5989eb9ee
jonathan45801 <jonathan45801@gmail.com>
2018-09-23 14:09:50 +0700

    boostrap udh kembali semula.tambahan tampilan login & tampilan timeline

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Sistem Informasi Kegiatan Mahasiswa IKOM</title>
-<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/tampilanlogin.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/tampilantimeline.min.css')}}">
