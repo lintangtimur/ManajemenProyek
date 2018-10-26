211167a890524e57383e5bcd7dd92d5ef62e0063
jonathan45801 <jonathan45801@gmail.com>
2018-10-27 00:50:33 +0700

    tampilan yang barunya untuk login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,30 +6,43 @@
     
     <link rel="stylesheet" href="{{asset('css/tampilantimeline.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/tampilanlogin.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    
     
 </head>
 <body>
-
-<div id="login">
         <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-4">
-                    <div id="login-box" class="col-md-8">
-                <h1>Login </h1><br>
-                <form action="/login" method="post">
+            <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                    <h3 class="panel-title">Login</h3>
+                </div>
+                <div class="panel-body">
+                <div class="row">
+                <div class="col-md-6 separator social-login-box"> <h4>Login Mahasiswa</h4><br>
+                    <a href="#" class="btn google btn-block" type="button"><li class="fab fa-google-plus-g "></li>Login With Google</a>
+                        </div>
+                <div class="col-sm-6 col-md-6 login-box">
+                <form action="/login" method="post" role="form">
                     {{ csrf_field() }}
+                    <h4>Login Untuk Dosen atau Ormawa </h4>
                     <div class="form-group">
                         <label for="user">Username</label>
-                        <input type="text" name="user" id="user" class="form-control" placeholder="masukkan username anda" aria-describedby="helpId">
+                        <input type="text" name="user" id="user" class="form-control" placeholder="masukkan username anda" aria-describedby="helpId" autofocus>
                     </div>
                     <div class="form-group">
                         <label for="pass">Password</label>
-                        <input type="password" name="password" id="pass" class="form-control" placeholder="pass" aria-describedby="helpId">                                  
+                        <input type="password" name="password" id="pass" class="form-control" placeholder="Masukkan Password anda" aria-describedby="helpId" >                                  
                     </div>
                    
                     <button type="submit" class="btn btn-info">LOGIN</button>
                     
                 </form>
+                </div>
+                </div>
+                </div>
+              
                 @include('partial.error')
                 
             </div>
@@ -38,67 +51,80 @@
 
 </div>
 </div>
-
+</div>
+<script>
+    $(document).ready(function(){
+    $(".social-login-box").height( $(".login-box").height() - 1000 );
+});
+</script>
 
             <!-- Upcoming event-->
     <div class="col-md-8">
     <header class="page-header">
         <h1>UpComing Event</h1>
     </header>
-  
-  <ul class="timeline">
-    <li><div class="tldate">september 2018</div></li>
-    
-    <li>
-      <div class="tl-circ"></div>
-      <div class="timeline-panel">
-        <div class="tl-heading">
-          <h4>MP</h4>
-          <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 25/9/2018</small></p>
-        </div>
-        <div class="tl-body">
-          <p>pertemuan 5 </p>
-        </div>
-      </div>
-    </li>
-    
-    <li class="timeline-inverted">
-      <div class="tl-circ"></div>
-      <div class="timeline-panel ">
-        <div class="tl-heading">
-          <h4>Project NASM Login</h4>
-          <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 23/9/2018</small></p>
-        </div>
-        <div class="tl-body">
-          <p>Tester Login</p>
-            
-        
-        </div>
-      </div>
-    </li>
-    
-    <li><div class="tldate">Oktober 2018</div></li>
-    
-    <li>
-      <div class="tl-circ"></div>
-      <div class="timeline-panel">
-        <div class="tl-heading">
-          <h4> SKS</h4>
-          <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i>1/10/2018 </small></p>
-        </div>
-        <div class="tl-body">
-          <p>Batas Bayar SKS tanpa denda</p>
-        </div>
-      </div>
-    </li>
-    
-    <li><div class="tldate">November 2018</div></li>
-    
-    
-    
-  </ul>
-  
-            </div>
+    <div class="main-timeline7">
+        <div class="timeline">
+           <div class="timeline-icon"><i class="fa fa-calendar"></i></div>
+              <span class="year">2018</span>
+                  <div class="timeline-content">
+                        <h5 class="title">Senin</h5>
+                          <p class="description">
+                                    WES BOLOS DISKRIT RAK ONO BAJU CUK
+                                </p>
+                            </div>
+                        </div>
+                        <div class="timeline">
+                            <div class="timeline-icon"><i class="fa fa-calendar"></i></div>
+                            <span class="year">2018</span>
+                            <div class="timeline-content">
+                                <h5 class="title">Selasa</h5>
+                                <p class="description">
+                                    WES NYAWA 0 DI MP BANGSAT TENAN
+                                </p>
+                            </div>
+                        </div>
+                        <div class="timeline">
+                            <div class="timeline-icon"><i class="fa fa-calendar"></i></div>
+                            <span class="year">2018</span>
+                            <div class="timeline-content">
+                                <h5 class="title">Rabu</h5>
+                                <p class="description">
+                                    KUMPULIN KKN CUK
+                                </p>
+                            </div>
+                        </div>
+                        <div class="timeline">
+                            <div class="timeline-icon"><i class="fa fa-calendar"></i></div>
+                            <span class="year">2018</span>
+                            <div class="timeline-content">
+                                <h5 class="title">Kamis</h5>
+                                <p class="description">
+                                    WES NYAWA 0 DI MP BANGSAT TENAN
+                                </p>
+                            </div>
+                        </div>
+                        <div class="timeline">
+                            <div class="timeline-icon"><i class="fa fa-calendar"></i></div>
+                            <span class="year">2018</span>
+                            <div class="timeline-content">
+                                <h5 class="title">Jumat</h5>
+                                <p class="description">
+                                    LIBUR JANCUK
+                                </p>
+                            </div>
+                        </div>
+                        <div class="timeline">
+                            <div class="timeline-icon"><i class="fa fa-calendar"></i></div>
+                            <span class="year">2018</span>
+                            <div class="timeline-content">
+                                <h5 class="title">Sabtu</h5>
+                                <p class="description">
+                                    LIBUR JANCUK
+                                </p>
+                            </div>
+                        </div>
+                    </div>
     </div>
 
     @include('partial.dependency')
