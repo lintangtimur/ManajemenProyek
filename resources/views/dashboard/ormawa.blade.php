7d8f458b03cabb4160e133ab58b144e7f47a3aa8
lintangtimur <lintangtimur915@gmail.com>
2018-11-10 21:28:30 +0700

    add modal view

--- resources/views/dashboard/ormawa.blade.php
+++ resources/views/dashboard/ormawa.blade.php
@@ -21,9 +21,60 @@
                         </div> --}}
                     </div>
                     <button type="button" class="btn btn-primary float-right ml-2">View</button>
-                    <button type="button" class="btn btn-primary float-right">Edit</button>
+                    <button type="button" class="btn btn-primary float-right btnEditOrmawa" data-toggle="modal" data-target="#exampleModal" data-acaraedit="{{$keg->id}}">Edit</button>
                 </div>
             </div>
     </div>
 </div>
 @endforeach
+
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <form action="kegiatan/upload" method="post" enctype="multipart/form-data">
+                {{ csrf_field() }}
+            <div class="card-body">
+                <h4 class="card-title">Edit Acara</h4>
+                <div class="form-group">
+                    <label for="namaAcara">Nama Acara</label>
+                    <input type="text" name="namaAcara" id="namaAcara" class="form-control" placeholder="masukkan nama acara">
+                </div>
+                <div class="form-group">
+                    <label for="temaAcara">Tema Acara</label>
+                    <input type="text" name="temaAcara" id="temaAcara" class="form-control" placeholder="masukkan tema acara">
+                </div>
+                <div class="form-group">
+                    <label for="tanggalAcara">Tanggal Acara</label>
+                    <input type="date" name="tanggalAcara" id="tanggalAcara" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="tempatAcara">Tempat Acara</label>
+                    <input type="text" name="tempatAcara" id="tempatAcara" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="berkasAcara">Upload Proposal Acara</label>
+                    <input type="file" name="berkasAcara" id="berkasAcara" class="form-control">
+                    <small class="text-muted">Maximum upload berkas 2MB</small>
+                </div>
+                <div class="form-group">
+                    <label for="anggaranAcara">Anggaran Acara</label>
+                    <input type="text" name="anggaranAcara" id="anggaranAcara" class="form-control">
+                </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save</button>
+        </div>
+    </form>
+      </div>
+    </div>
+  </div>
