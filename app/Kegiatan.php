82c66657b0204d3499fa1af831f1a158a39669fd
lintangtimur <lintangtimur915@gmail.com>
2018-11-10 21:29:36 +0700

    add totalApproved(), approved(), protected TanggalAcara

--- app/Kegiatan.php
+++ app/Kegiatan.php
@@ -6,7 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kegiatan extends Model
 {
+    protected $dates = ['tanggalAcara'];
     protected $fillable = [
         'uploaderId', 'namaAcara', 'temaAcara', 'tanggalAcara', 'tempatAcara', 'fileName', 'pathFile', 'anggaran'
     ];
+
+    /**
+     * Mendapatkan total berapa proposal yang sudah di approved oleh dosen
+     *
+     * @return void
+     */
+    public function totalApproved()
+    {
+        return Kegiatan::where('status', '=', 1)->count();
+    }
+
+    /**
+     * Mendapatkan kegiatan yang sudah di approved oleh dosen
+     *
+     * @return void
+     */
+    public function approved()
+    {
+        return Kegiatan::where('status', '=', 1)->get();
+    }
+
+    // public function getTanggalAcaraAttribute()
+    // {
+    //     return $this->attributes['tanggalAcara']->format('m/d/Y');
+    // }
 }
