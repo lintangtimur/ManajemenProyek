704dd593de5da6f4b096a4f6d12cd617ebe550cd
lintangtimur <lintangtimur915@gmail.com>
2018-11-10 21:30:56 +0700

    revision migration for Decline Dosen

---
+++ database/migrations/2018_11_10_130231_create_revisions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRevisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('revisions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idAcara');
+            $table->string('comment', 100);
+            $table->string('commentId', 100);
+            $table->boolean('status')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('revisions');
+    }
+}
