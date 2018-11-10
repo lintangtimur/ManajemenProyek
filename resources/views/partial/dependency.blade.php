06f71e730151372eabdc967eb6b7d8a08ddb20d1
lintangtimur <lintangtimur915@gmail.com>
2018-11-10 21:26:03 +0700

    add datatable dependency

--- resources/views/partial/dependency.blade.php
+++ resources/views/partial/dependency.blade.php
@@ -3,4 +3,5 @@
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
 <script src="{{url('js/jquery.number.min.js')}}"></script>
+<script src="{{url('js/datatables.min.js')}}"></script>
 {{-- <script src="{{url('js/bootstrap.min.js')}}"></script> --}}
--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -6,6 +6,7 @@
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 <link rel="stylesheet" href="{{asset('css/sidebar.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/datatables.min.css')}}">
 
 
 <style>
